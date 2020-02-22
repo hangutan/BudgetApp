00f7f647b911db43215e851a66c4a90bfe6a0a18
unknown <hangutan>
2020-02-22 23:39:50 +0700

    Updates

---
+++ precache-manifest.53b5960773b57e1a3ead7fab9e42b207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de2589a0d3051f4c436bb1641334f41",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "0140553af4b1b7135a44",
+    "url": "/BudgetApp/static/css/main.74a4c563.chunk.css"
+  },
+  {
+    "revision": "6e23e575a20cb71aa4b9",
+    "url": "/BudgetApp/static/js/2.86572c82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/BudgetApp/static/js/2.86572c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0140553af4b1b7135a44",
+    "url": "/BudgetApp/static/js/main.99cae960.chunk.js"
+  },
+  {
+    "revision": "2caa1df62dbf60643c51",
+    "url": "/BudgetApp/static/js/runtime-main.f6acf065.js"
+  }
+]);
